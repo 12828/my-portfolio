9d55a320dd7759f40ddc8f56f620462e56ca1be0
12828 <71511300+12828@users.noreply.github.com>
2020-11-13 10:50:37 -0500

    new change

--- styles/styles.js
+++ styles/styles.js
@@ -1,8 +1,1 @@
-function greet(){
-    alert("Hi! Welcome to my website");
-  }
-  
-  function getName(){
-    let name = prompt("What's your name?");
-    alert("Nice to meet you, " + name);
-  }
+
